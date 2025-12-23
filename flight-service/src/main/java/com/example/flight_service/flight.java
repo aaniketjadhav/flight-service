fdd09082bf16a99d53d906bf06b43078e1e6d646
aaniketjadhav <aaniketjadhav63@gmail.com>
2025-12-23 23:39:18 +0530

    code refactor

---
+++ flight-service/src/main/java/com/example/flight_service/flight.java
@@ -0,0 +1,16 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class train {
+
+    @GetMapping("/flight")
+    public String getData() {
+        return "Please book all your flight from makemytip at 20% discount";
+    }
+
+    @GetMapping("/spicejetflight")
+    public String putData() {
+        return "Please book all your flight from makemytip at 20% discount";
+    }
+}
